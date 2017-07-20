a1827a2f7220ecfe83f19705783253ccd4298bc2
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2017-07-20 20:10:19 +0500

    comments removed

--- src/screen/desktoptabs/profiletab.js
+++ src/screen/desktoptabs/profiletab.js
@@ -1,35 +1,3 @@
-// import React, { Component } from 'react';
-// import {
-//     View,
-// } from 'react-native'
-// export default class ProfileTab extends Component {
-//   render() {
-//     return (
-//       <View>
-//           <Card>
-//             <CardItem header>
-//               <Text>NativeBase</Text>
-//             </CardItem>
-//             <CardItem>
-//               <Body>
-//                 <Text>
-//                   Your text here
-//                 </Text>
-//               </Body>
-//             </CardItem>
-//             <CardItem footer>
-//               <Text>
-//                 GeekyAnts
-//               </Text>
-//             </CardItem>
-//          </Card>
-
-//       </View>
-//     );
-//   }
-// }
-
-
 import React, { Component } from 'react';
 import  {
     Text,
@@ -37,24 +5,17 @@ import  {
     ScrollView,
     StyleSheet 
 } from 'react-native';
-
 import { Card, ListItem, Button, Divider, Avatar, FormLabel, FormInput } from 'react-native-elements'
+
 export default class ProfileTab extends Component {
     render() {
-
-const users = [
-            {
-            name: 'brynn',
-            avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
-        }];
         return (
         <ScrollView ref={(scrollView) => { _scrollView = scrollView; }} automaticallyAdjustContentInsets={true}>
           <Card title='Hello Doc!'>
             <View style={{marginLeft: 100}}>
                 <Avatar xlarge rounded activeOpacity={0.7}
                     source={{uri: "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg"}}
-                    onPress={() => console.log("Works!")}
-                />
+                    onPress={() => console.log("Works!")}/>
             </View>
             <Text style={styles.DocName}>
                 Doc. Hensienberg Patrick
