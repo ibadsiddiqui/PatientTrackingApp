9bfde12daf26a35d6d72b5c8735025336a2e2124
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2017-07-20 20:06:20 +0500

    Component inserted to desktop screen

--- src/screen/desktop.js
+++ src/screen/desktop.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet,
-  ScrollView,
-  Text,
   View,
-  Image,
 } from 'react-native';
-import {StackNavigator} from 'react-navigation';
+import ProfileTab from './desktoptabs/profiletab'
+
 import ScrollableTabView, {DefaultTabBar} from 'react-native-scrollable-tab-view';
 
 export default class Desktop extends Component {
@@ -16,31 +13,22 @@ export default class Desktop extends Component {
         position: 'absolute',
         backgroundColor: 'transparent',
         zIndex: 100,
-
       },
       headerTintColor:"#bdbdbd"
     });
-    constructor(){
-        super();
-    }
-
-    handleSubmit(e){
-        e.preventDefault();
-    }
-
     render() {
         return (             
                 <ScrollableTabView tabBarBackgroundColor ="transparent" tabBarActiveTextColor ="navy" 
-                        tabBarPosition="overlayBottom"
+                        tabBarPosition="top"
                         ref={(tabView) => { this.tabView = tabView; }}>
                     <View tabLabel='Profile'>
-                       
+                       <ProfileTab/>
                     </View>
                     <View tabLabel='Insert'>
                         
                     </View>
                     <View tabLabel='View History'>
-                        
+                    
                     </View>
                     <View tabLabel='Search'>
                         
@@ -49,10 +37,3 @@ export default class Desktop extends Component {
         );
     }
 }
-
-const styles = StyleSheet.create({
-  backgroundImg:{
-height: 660,
-        width:420
-    },
-});
