3d7adca33c9816013ab2e9caba85c1bbe41e9bd5
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2017-07-22 00:06:47 +0500

    UI changed of Search component

--- src/screen/desktoptabs/searchtab.js
+++ src/screen/desktoptabs/searchtab.js
@@ -5,20 +5,39 @@ import  {
     ScrollView,
     StyleSheet 
 } from 'react-native';
-import { Card, FormInput, FormLabel, Divider } from 'react-native-elements'
+import { Card, FormInput, FormLabel, Divider, List, ListItem } from 'react-native-elements'
 
 export default class SearchTab extends Component {
     render() {
+        const list = [
+            {
+                name: 'Amy Farha',
+                avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+                subtitle: 'Vice President'
+            },
+            {
+                name: 'Chris Jackson',
+                avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+                subtitle: 'Vice Chairman'
+            },]
+
         return (
         <ScrollView ref={(scrollView) => { _scrollView = scrollView; }} automaticallyAdjustContentInsets={true}>
             <Card >
                 <FormLabel>Search</FormLabel>
-                <FormInput/>
+                <FormInput placeholder="Search by date or Name"/>
             </Card>
             <Divider style={{marginTop: 20, marginLeft: 66,backgroundColor: 'black', width: 300}} />
             <Card >
-                <FormLabel>Search</FormLabel>
-                <FormInput/>
+                <List containerStyle={{marginBottom: 20}}>
+            {list.map((l, i) => (
+                <ListItem
+                    roundAvatar
+                    avatar={{uri:l.avatar_url}}
+                    key={i}
+                    title={l.name}/>
+            ))}
+            </List>
             </Card>
         </ScrollView>
             
