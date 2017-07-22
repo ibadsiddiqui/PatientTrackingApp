eb61c19fdeb65a9e5cf19702ce95c95daa97ea91
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2017-07-22 22:51:58 +0500

    firebase app confog added

--- index.android.js
+++ index.android.js
@@ -6,5 +6,14 @@ import {
   View
 } from 'react-native';
 import PatientTrackApp from './src/screen/main'
-
+import firebase from 'firebase'
+var config = {
+    apiKey: "AIzaSyCwFZ1vHAcTGB8YrqqEBFAYVO8mT2LWVhU",
+    authDomain: "loginsignupnative.firebaseapp.com",
+    databaseURL: "https://loginsignupnative.firebaseio.com",
+    projectId: "loginsignupnative",
+    storageBucket: "loginsignupnative.appspot.com",
+    messagingSenderId: "331903238162"
+  };
+  firebase.initializeApp(config);
 AppRegistry.registerComponent('PatientTrackApp', () => PatientTrackApp);
